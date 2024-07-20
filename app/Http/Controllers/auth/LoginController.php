04a8a2ecf799d0ed75bb0e46e43ec6a602deb765
Ahmed Gomaa <ahmedgfathy@gmail.com>
2024-07-20 12:10:19 +0300

    first_creation_upload

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
